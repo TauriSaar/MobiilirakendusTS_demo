2bf5f8c47de892bda8dea2a5ac03f96a520613de
TauriSaar <94620796+TauriSaar@users.noreply.github.com>
2024-02-09 09:29:06 +0200

    Splash screen done

--- src/screens/auth/Splash/index.js
+++ src/screens/auth/Splash/index.js
@@ -2,16 +2,17 @@ import React from "react";
 import {
     Text,
     View,
-    Image,
+    Image
 } from "react-native";
+import {styles} from "./styles";
 
 const Splash = () => {
     return (
-        <View>
-            <Image
-                source={require("../../../assets/splash_image.png")}
-            />
-            <Text>You'll Find All You Need Here!</Text>
+        <View style={styles.container}>
+            <Image resizeMode="contain" style={styles.image} source={require("../../../assets/splash_image.png")}/>
+            <Text style={styles.title}>You'll Find</Text>
+            <Text style={[styles.title, styles.innerTitle]}>All You Need</Text> 
+            <Text style={styles.title}>Here!</Text>
         </View>
     )
 }
---
+++ src/screens/auth/Splash/styles.js
@@ -0,0 +1,21 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 200
+    },
+    title: {
+        fontSize: 40,
+        fontWeight: 'bold'
+    },
+    innerTitle: {
+        color: '#FCA34D',
+        textDecorationLine: 'underline'
+    },
+    container: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+    }
+});
