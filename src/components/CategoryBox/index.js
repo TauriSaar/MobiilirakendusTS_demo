1fbc950b69c440974956ad7ed1f45656aaf4280c
TauriSaar <94620796+TauriSaar@users.noreply.github.com>
2024-02-26 09:35:22 +0200

    Filtreerimine kategooria järgi

--- src/components/CategoryBox/index.js
+++ src/components/CategoryBox/index.js
@@ -2,13 +2,15 @@ import React from "react";
 import { Pressable, Image, Text, View } from "react-native";
 import { styles } from "./styles";
 
-const CategoryBox = ({ title, image, onPress }) => {
+const CategoryBox = ({ title, image, onPress, isSelected }) => {
   return (
     <Pressable onPress={onPress} style={styles.container}>
-      <View style={styles.imageContainer}>
+      <View style={[styles.imageContainer, isSelected ? { backgroundColor: 'black' } : {}]}>
         <Image style={styles.image} source={{ uri: image }} />
       </View>
-      <Text style={styles.title}>{title}</Text>
+      <Text style={[styles.title, isSelected ? { color: 'blue', fontWeight: '500' } : {}]}>
+        {title}
+      </Text>
     </Pressable>
   );
 };
--- src/screens/app/Home/index.js
+++ src/screens/app/Home/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, FlatList } from "react-native"
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { styles } from "./styles";
@@ -9,15 +9,30 @@ import { products } from "../../../data/products";
 import ProductHomeItem from "../../../components/ProductHomeItem";
 
 const Home = () => {
+    const [selectedCategory, setSelectedCategory] = useState()
+    const [selectedProduct, setSelectedProduct] = useState(products)
+
+    useEffect(() => {
+        if (selectedCategory) {
+          const updatedSelectedProducts = products.filter((product) =>
+            product?.category === selectedCategory
+          );
+          setSelectedProduct(updatedSelectedProducts);
+        } else {
+            setSelectedProduct(products);
+        }
+      }, [selectedCategory]);
+      
+
     const renderCategoryItem = ({item}) => {
-        console.log('item => ', item)
         return (
-            <CategoryBox title={item?.title} image={item?.image} />
+            <CategoryBox 
+            onPress={() => setSelectedCategory(item?.id)}
+            isSelected={item?.id === selectedCategory} title={item?.title} image={item?.image} />
         )
     }
 
     const renderProductItem = ({item}) => {
-        console.log('item => ', item)
         return (
             <ProductHomeItem {...item} />
         )
@@ -29,7 +44,7 @@ const Home = () => {
                 <Header showSearch={true} title="Find All You Need" />
                 <FlatList style={styles.list} horizontal data={categories} renderItem={renderCategoryItem} keyExtractor={(item, index) =>
                 String(index)}/>
-                <FlatList numColumns={2} key={'item'} data={products} renderItem={renderProductItem} 
+                <FlatList numColumns={2} key={'item'} data={selectedProduct} renderItem={renderProductItem} 
                 keyExtractor={(item) => String(item.id)} ListFooterComponent={<View style={{height: 250}}/>} />
             </View>
         </SafeAreaView>
